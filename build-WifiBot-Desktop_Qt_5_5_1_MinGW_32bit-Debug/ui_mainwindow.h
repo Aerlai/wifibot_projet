4d85cb48aa3d818dd925fb43963e35c4ea4ec24b
eloslegallois <gall.antoine.ag@gmail.com>
2016-02-12 15:40:56 +0100

    interface pour le socket
    interface graphique pour le socket

---
+++ build-WifiBot-Desktop_Qt_5_5_1_MinGW_32bit-Debug/ui_mainwindow.h
@@ -0,0 +1,149 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.5.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QFormLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTabWidget>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *actionConnection;
+    QAction *actionConfigurer;
+    QWidget *centralWidget;
+    QTabWidget *tabWidget;
+    QWidget *Module;
+    QWidget *tab_connexion;
+    QWidget *formLayoutWidget;
+    QFormLayout *formLayout_2;
+    QLabel *label;
+    QLineEdit *lineEdit;
+    QLabel *label_2;
+    QLineEdit *lineEdit_2;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(1000, 800);
+        QSizePolicy sizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(MainWindow->sizePolicy().hasHeightForWidth());
+        MainWindow->setSizePolicy(sizePolicy);
+        MainWindow->setMinimumSize(QSize(1000, 800));
+        MainWindow->setMaximumSize(QSize(1000, 800));
+        QPalette palette;
+        QBrush brush(QColor(255, 255, 255, 255));
+        brush.setStyle(Qt::SolidPattern);
+        palette.setBrush(QPalette::Active, QPalette::Base, brush);
+        QBrush brush1(QColor(240, 235, 206, 255));
+        brush1.setStyle(Qt::SolidPattern);
+        palette.setBrush(QPalette::Active, QPalette::Window, brush1);
+        palette.setBrush(QPalette::Inactive, QPalette::Base, brush);
+        palette.setBrush(QPalette::Inactive, QPalette::Window, brush1);
+        palette.setBrush(QPalette::Disabled, QPalette::Base, brush1);
+        palette.setBrush(QPalette::Disabled, QPalette::Window, brush1);
+        MainWindow->setPalette(palette);
+        MainWindow->setAcceptDrops(false);
+        actionConnection = new QAction(MainWindow);
+        actionConnection->setObjectName(QStringLiteral("actionConnection"));
+        actionConfigurer = new QAction(MainWindow);
+        actionConfigurer->setObjectName(QStringLiteral("actionConfigurer"));
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        tabWidget = new QTabWidget(centralWidget);
+        tabWidget->setObjectName(QStringLiteral("tabWidget"));
+        tabWidget->setGeometry(QRect(630, 20, 361, 391));
+        Module = new QWidget();
+        Module->setObjectName(QStringLiteral("Module"));
+        tabWidget->addTab(Module, QString());
+        tab_connexion = new QWidget();
+        tab_connexion->setObjectName(QStringLiteral("tab_connexion"));
+        formLayoutWidget = new QWidget(tab_connexion);
+        formLayoutWidget->setObjectName(QStringLiteral("formLayoutWidget"));
+        formLayoutWidget->setGeometry(QRect(10, 10, 331, 341));
+        formLayout_2 = new QFormLayout(formLayoutWidget);
+        formLayout_2->setSpacing(6);
+        formLayout_2->setContentsMargins(11, 11, 11, 11);
+        formLayout_2->setObjectName(QStringLiteral("formLayout_2"));
+        formLayout_2->setContentsMargins(0, 0, 0, 0);
+        label = new QLabel(formLayoutWidget);
+        label->setObjectName(QStringLiteral("label"));
+        QSizePolicy sizePolicy1(QSizePolicy::Preferred, QSizePolicy::Minimum);
+        sizePolicy1.setHorizontalStretch(0);
+        sizePolicy1.setVerticalStretch(0);
+        sizePolicy1.setHeightForWidth(label->sizePolicy().hasHeightForWidth());
+        label->setSizePolicy(sizePolicy1);
+
+        formLayout_2->setWidget(0, QFormLayout::LabelRole, label);
+
+        lineEdit = new QLineEdit(formLayoutWidget);
+        lineEdit->setObjectName(QStringLiteral("lineEdit"));
+
+        formLayout_2->setWidget(0, QFormLayout::FieldRole, lineEdit);
+
+        label_2 = new QLabel(formLayoutWidget);
+        label_2->setObjectName(QStringLiteral("label_2"));
+
+        formLayout_2->setWidget(1, QFormLayout::LabelRole, label_2);
+
+        lineEdit_2 = new QLineEdit(formLayoutWidget);
+        lineEdit_2->setObjectName(QStringLiteral("lineEdit_2"));
+
+        formLayout_2->setWidget(1, QFormLayout::FieldRole, lineEdit_2);
+
+        tabWidget->addTab(tab_connexion, QString());
+        MainWindow->setCentralWidget(centralWidget);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        retranslateUi(MainWindow);
+
+        tabWidget->setCurrentIndex(1);
+
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "WifiBot", 0));
+        actionConnection->setText(QApplication::translate("MainWindow", "Connection", 0));
+        actionConfigurer->setText(QApplication::translate("MainWindow", "Configurer", 0));
+        tabWidget->setTabText(tabWidget->indexOf(Module), QApplication::translate("MainWindow", "Modules", 0));
+        label->setText(QApplication::translate("MainWindow", "adresse IP", 0));
+        label_2->setText(QApplication::translate("MainWindow", "Port", 0));
+        tabWidget->setTabText(tabWidget->indexOf(tab_connexion), QApplication::translate("MainWindow", "Connexion", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
