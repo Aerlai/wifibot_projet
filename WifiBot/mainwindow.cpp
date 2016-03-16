17e08e129ed852a5b7f86c82b73c69fc613c18a2
JulienGaudillat <gaudillat.julien@gmail.com>
2016-03-16 15:28:47 +0100

    Interface graphique v2

--- WifiBot/mainwindow.cpp
+++ WifiBot/mainwindow.cpp
@@ -20,3 +20,43 @@ void MainWindow::on_pushButton_clicked()
 {
 
 }
+
+void MainWindow::keyPressEvent(QKeyEvent* event) {
+
+    switch(event->key()){
+    case Qt::Key_Z:
+        ui->pushButton_7->animateClick();
+        break;
+    case Qt::Key_S:
+        ui->pushButton_3->animateClick();
+        break;
+    case Qt::Key_Q:
+        ui->pushButton_5->animateClick();
+        break;
+    case Qt::Key_D:
+        ui->pushButton_6->animateClick();
+        break;  
+    case Qt::Key_K:
+        ui->pushButton_10->animateClick();
+        break;
+    case Qt::Key_J:
+        ui->pushButton_12->animateClick();
+        break;
+    case Qt::Key_L:
+        ui->pushButton_8->animateClick();
+        break;
+    case Qt::Key_I:
+        ui->pushButton_9->animateClick();
+        break;
+    case Qt::Key_0:
+        ui->pushButton_4->animateClick();
+        break;
+
+    }
+
+}
+
+void MainWindow::keyReleaseEvent(QKeyEvent* event) {
+    setWindowTitle(tr("keyReleaseEvent!!"));
+}
+
